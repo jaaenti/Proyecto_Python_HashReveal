b94a50d479aa19e3c6b34953e2e303a87caf0280
jaaenti <joan.aguilar@enti.cat>
2024-12-10 12:34:59 +0100

    Update main.py

--- interface.py
+++ interface.py
@@ -7,8 +7,8 @@ print("""
 
 |
 |
-|-----> Autor: JaaEnti
-|-----> Version: 0.0.0
+|-----> Autor: JaaEnti, Pakhusso
+|-----> Version: 1.0.0
 |-----> https://github.com/jaaenti 
 |                                                                                                                                                                
 """)
