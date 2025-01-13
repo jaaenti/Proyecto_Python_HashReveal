2e185878673559dc285adac52c4dd0a76e7c4839
Pakhusso <daniel.diaz@enti.cat>
2025-01-13 21:40:05 +0100

    SCRUM-32

--- modules/type_detect_hash.py
+++ modules/type_detect_hash.py
@@ -1,4 +1,5 @@
 from hashid import HashID
+from modules.type_navigation import navigation_menu
 
 TRUSTED_LENGTHS = {
     32: ["MD5 or NTLM"],
@@ -9,7 +10,7 @@ TRUSTED_LENGTHS = {
 
 def detect_hashid(hash_value):
     hashid = HashID()
-    results = list(hashid.identifyHash(hash_value))  
+    results = list(hashid.identifyHash(hash_value))
 
     if "$argon2" in hash_value:
         return "Argon2"
@@ -29,27 +30,6 @@ def detect_hashid(hash_value):
     
     return "Unknown Hash"
 
-def navgation_menu():
-    while True:
-        print("\nWhat do you want to do next?")
-        print("1. Detect another hash")
-        print("2. Go to the next module")
-        print("3. Go to the next module")
-        print("4. Exit program")
-        choice = input("Enter your choice: ").strip()
-        
-        if choice == '1':
-            return "retry"
-        elif choice == '2':
-            return "next_module"
-        elif choice == '3':
-            return "next_module2"
-        elif choice == '4':
-            print("Exiting the program. Goodbye!")
-            return "exit"
-        else:
-            print("Invalid input. Please enter '1', '2', '3' or '4'.")
-
 def detect_hash():
     while True:
         hash_value = input("Enter the hash to detect: ").strip()
@@ -60,14 +40,20 @@ def detect_hash():
         hash_type = detect_hashid(hash_value)
         print(f"\nHash Detected: {hash_type}\n")
 
-        action = navgation_menu()
-        if action == "retry":
+        action = navigation_menu()
+        if action == "detect":
+            print("Restarting Hash Detection...")
             continue
-        elif action == "next_module":
-            print("Going to the next module... (implement this functionality here)")
+        elif action == "encrypt":
+            from modules.type_generate_hash import generate_hash
+            print("Going to Encrypt Hash module...")
+            generate_hash()  # Llama al módulo de generación de hash
+            break
+        elif action == "decrypt":
+            from modules.type_decrypt_hash import decrypt_hash
+            print("Going to Decrypt Hash module...")
+            decrypt_hash()  # Llama al módulo de desencriptar hash
             break
         elif action == "exit":
+            print("Exiting the program. Goodbye!")
             break
-
-if __name__ == "__main__":
-    detect_hash()
